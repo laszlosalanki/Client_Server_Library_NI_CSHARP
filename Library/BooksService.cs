2e57c22236ac08f32d393eca1acec8bdfbaf46ca
ktn-andrea <ktn.andrea@gmail.com>
2022-01-08 21:13:37 +0100

    Init service

---
+++ Library/BooksService.cs
@@ -0,0 +1,19 @@
+﻿internal class BooksService
+{
+    private static BooksService? _instance;
+    public static BooksService GetInstance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new BooksService();
+            }
+            return _instance;
+        }
+    }
+
+    public string SayHello() {
+        return "Hello world.";
+    }
+}
--- Library/Program.cs
+++ Library/Program.cs
@@ -1,32 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// init service
+var booksService = BooksService.GetInstance;
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
-var summaries = new[]
+app.MapGet("test", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
+    return booksService.SayHello();
 });
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
