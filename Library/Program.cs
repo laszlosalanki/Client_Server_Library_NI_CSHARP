7caee6d961ba0a04c8393e77a039ace1f571aa13
ktn-andrea <ktn.andrea@gmail.com>
2022-01-10 02:21:44 +0100

    CORS config

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,17 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<IBookRepository, BookRepository>();
 
 builder.Services.AddMvc();
+builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddControllers();
 builder.Services.AddDbContext<BookContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlDb")));
 
 var app = builder.Build();
 
+app.UseCors(o => o.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllers();
